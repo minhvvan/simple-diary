ea9bd1f3730a4a855ede27a0d11d289cf5dd3e27
unknown <eminhwan12@gmail.com>
2020-08-15 18:22:00 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { CalendarList } from 'react-native-calendars';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <CalendarList
+        current={'2020-08-15'}
+        minDate={'2012-05-10'}
+        maxDate={'2052-05-30'}
+        onDayPress={(day) => {console.log('selected day', day)}}
+        onDayLongPress={(day) => {console.log('selected day', day)}}
+        firstDay={1}
+        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+        pastScrollRange={50}
+        futureScrollRange={50}
+        scrollEnabled={true}
+        showScrollIndicator={true}
+    />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +28,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
 });
